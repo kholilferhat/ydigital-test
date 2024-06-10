0391b912d576fc2dad9815e5e7f50d83fbf66687
kholil <kholilferhat@gmail.com>
2024-06-10 12:32:27 +0700

    fix: welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,5 +2,17 @@
 
 @section('content')
 
-    <h1>Welcome</h1>
+    <section class="welcome-section">
+        <h1>Selamat Datang</h1>
+        <div class="cards">
+            <a href="/acne-type" class="card-container">
+                <img src="/assets/2-program-kategori-banner-mobile-1.avif" alt="banner acne type erha">
+                <p>Acne Type</p>
+            </a>
+            <a href="/doctor/1"  class="card-container">
+                <img src="/assets/doctor-1.png" alt="banner acne type erha">
+                <p>dr. Stella Adriana Limanjaya</p>
+            </a>
+        </div>
+    </section>
  @endsection      
